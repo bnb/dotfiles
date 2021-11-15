e1ad4eed16704771eaa4eac4486ec9ae1e043420
Tierney Cyren <hello@bnb.im>
2021-11-15 17:00:10 -0500

    feat: add ff commands
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- configs/zsh/general/aliases.sh
+++ configs/zsh/general/aliases.sh
@@ -2,3 +2,5 @@
 alias r="code-insiders -r" # open the passed path in the current VS Code Insiders window, replacing the current contents
 alias a="code-insiders -a" # open the passed path in the current VS Code Insiders workspace
 alias i="npx covgen hello@bnb.im && npx license mit && npx gitignore node &&  echo \"package-lock=false\" > .npmrc" # init new project files that can't be init'd by an init command
+alias mainff="git fetch upstream && git checkout main && git merge upstream/main"
+alias masterff="git fetch upstream && git checkout maseter && git merge upstream/master"
