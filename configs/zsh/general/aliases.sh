d0ea68342df37e943f19782a4f1a96fac532fe7d
Tierney Cyren <hello@bnb.im>
2021-11-29 11:31:00 -0500

    chore: align ff commands
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- configs/zsh/general/aliases.sh
+++ configs/zsh/general/aliases.sh
@@ -2,6 +2,6 @@
 alias r="code-insiders -r" # open the passed path in the current VS Code Insiders window, replacing the current contents
 alias a="code-insiders -a" # open the passed path in the current VS Code Insiders workspace
 alias i="npx covgen hello@bnb.im && npx license mit && npx gitignore node &&  echo \"package-lock=false\" > .npmrc" # init new project files that can't be init'd by an init command
-alias mainff="git fetch upstream && git checkout main && git merge upstream/main"
-alias masterff="git fetch upstream && git checkout master && git merge upstream/master"
+alias ff:main="git fetch upstream && git checkout main && git merge upstream/main"
+alias ff:master="git fetch upstream && git checkout master && git merge upstream/master"
 alias dotfiles:update="git -C $PERSONAL_DOTFILES_PATH pull"
