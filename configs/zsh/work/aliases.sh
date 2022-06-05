be1f5bf6978c9670b9cd878bb42888832a6770e8
Tierney Cyren <hello@bnb.im>
2022-06-05 19:45:06 -0400

    chore: remove Microsoft employee stuff
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- configs/zsh/work/aliases.sh
+++
@@ -1,7 +0,0 @@
-  if [ -n "$PERSONAL_SETUP_DEBUG" ]
-  then
-    echo "setting up idweb command"
-  fi
-
-# work alias that opens up idweb on macOS
-alias idweb='kdestroy --all; kinit --keychain ticyren@NORTHAMERICA.CORP.MICROSOFT.COM; open http://idweb -a Safari.app' # Hack to make Microsoft's internal tool IDWeb work on macOS. This is a horrible hack. Also the alias included in it is not really confidential since you could figure it out pretty easily – I prefer not to use security through obscurity, especially if it makes my life easier not to do so.
