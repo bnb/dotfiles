8832751157a53b6acc59a75dd0e318dc9457d887
Tierney Cyren <hello@bnb.im>
2022-07-02 23:44:37 -0400

    feat: add upgrade commands for Ubuntu on WSL
    Signed-off-by: Tierney Cyren <hello@bnb.im>

---
+++ setup/wsl/fix-ubuntu.sh
@@ -0,0 +1,2 @@
+# this command kills snapd
+DEBIAN_FRONTEND=noninteractive sudo apt-get purge snapd -y  
---
+++ setup/wsl/update-ubuntu.sh
@@ -0,0 +1,6 @@
+# idk a bunch of commands that a bunch of different sources on the internet agree on
+sudo apt update 
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo apt install update-manager-core
+sudo do-release-upgrade
